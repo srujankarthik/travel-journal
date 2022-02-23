339409a1cfc63dba10fb7d58c60d49252bab3c5d
srujan <srujan.sharma77k@gmail.com>
2022-02-23 11:02:50 +1100

    added data.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import data from './data';
 
 function App() {
+  console.log(data);
   return (
     <div className="App">
       <h1>init setup</h1>
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        id: 1,
+        title: "Nouméa",
+        location: "New Caledonia",
+        googleMapsUrl: "https://goo.gl/maps/7ko8AyLctGjzfBkv6",
+        startDate: "15 Nov, 2024",
+        endDate: "29 Nov, 2024",
+        description: "I'll let chef explain this to me when he takes me through his country.",
+        imageUrl: "https://unsplash.com/photos/Zt5QdNc3zAs"
+    },
+
+    {
+        id: 2,
+        title: "Higashiyama Ward",
+        location: "Kyoto, Japan",
+        googleMapsUrl: "https://goo.gl/maps/E3WefWqE5uXHS44r5",
+        startDate: "1 Dec, 2024",
+        endDate: "19 Dec, 2024",
+        description: "Higashiyama-ku preserves the look and feel of feudal-era Japan in a wood-built historic quarter of narrow lanes lined with traditional pottery stores, tea houses, and restaurants for multi-course kaiseki meals. Centuries-old Zen and Shinto sites include lantern-lit Yasaka Shrine and hillside temple Kiyomizudera, which overlooks the city.",
+        imageUrl: "https://unsplash.com/photos/5iPIELR9SAI"
+    },
+
+    {
+        id: 3,
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2025",
+        endDate: "24 Jan, 2025",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas"
+    }
+]
+
