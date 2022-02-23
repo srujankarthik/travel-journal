97a69f6d67cb0f327b763237fac27c57c02d2b31
srujan <srujan.sharma77k@gmail.com>
2022-02-23 16:00:43 +1100

    Added components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import './App.css';
 import data from './data';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
 
 function App() {
   console.log(data);
   return (
     <div className="App">
+      <Navbar />
       <h1>init setup</h1>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Card(props) {
+    return (
+        <div>
+            <h1>Here's a Card Component</h1>
+            <img />
+        </div>
+    )
+}
