88584881720c4bfc43c370c4b33a746faaaca285
srujan <srujan.sharma77k@gmail.com>
2022-02-24 18:10:18 +1100

    finished styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,18 @@ import React from 'react'
 
 export default function Card(props) {
     return (
-        <div>
-            <img src={`../images/${props.item.imageUrl}`} width="400px" height="350px"/>
-            <h4>{props.item.location}</h4>
-            <a href={props.item.googleMapsUrl} target="_blank">View on Google Maps</a>
-            <h2>{props.item.title}</h2>
-            <h4>{props.item.startDate} - {props.item.endDate}</h4>
-            <p>{props.item.description}</p>
+        <div className="card">
+            <img src={`../images/${props.item.imageUrl}`} alt="" width="400px" height="350px" className="card-image" />
+            <div className="card-components">
+                <div className="card-location">
+                    <img src="../images/location.svg" alt="" width="15px" />
+                    <h4>{props.item.location}</h4>
+                    <a href={props.item.googleMapsUrl} target="_blank" rel="noopener noreferrer">View on Google Maps</a>
+                </div>
+                <h2 className="card-title">{props.item.title}</h2>
+                <h4 className="card-dates">{props.item.startDate} - {props.item.endDate}</h4>
+                <p className="card-description">{props.item.description}</p>
+            </div>
         </div>
     )
 }
